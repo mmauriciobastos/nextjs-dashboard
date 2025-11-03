7129e31970849c335b94a38218d4a19a92ab6d88
mmauriciobastos <mauriciofib@gmail.com>
2025-11-03 11:31:34 -0800

    chore: add try/catch to actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,10 +31,16 @@ export async function createInvoice(formData: FormData) {
   
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    console.log('Error creating invoice:', error);
+    console.error('Error creating invoice:', error);
+    return { message: 'Failed to create invoice.' };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -52,12 +58,17 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    console.error('Error updating invoice:', error);
+    return { message: 'Failed to update invoice.' };
+  } 
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
